6308d0d33fc1eefd7d6d14584e473da085d006f9
ohmygymofficial <contact@ohmygym.fr>
2019-06-26 01:02:18 +0200

    Import all Icon app size into Xcode Project

---
+++ Instagrid/Instagrid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by E&M Life Project on 26/06/2019.
+//  Copyright © 2019 Erwan PASTE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
