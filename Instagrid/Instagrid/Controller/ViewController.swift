f1cf5154e62a0c48e858e09c860834c80d6c26f9
ohmygymofficial <contact@ohmygym.fr>
2019-11-11 14:40:29 +0100

    create button with PLUS and RECTANGLE background in Square stackView, create an UIView parent for this stack view to have background

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // All objects of the view
+    
+    @IBOutlet weak var instagrid: UIImageView!
+    @IBOutlet weak var shareview: UIStackView!
+    @IBOutlet weak var swipeUpLabel: UILabel!
+    @IBOutlet weak var arrowUpImage: UIImageView!
+    @IBOutlet weak var squareView: UIStackView!
+    @IBOutlet weak var squareUpView: UIStackView!
+    @IBOutlet weak var squareBottomView: UIStackView!
+    @IBOutlet weak var chooseView: UIStackView!
+    @IBOutlet weak var layoutImage1: UIImageView!
+    @IBOutlet weak var layoutImage2: UIImageView!
+    @IBOutlet weak var layoutImage3: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
